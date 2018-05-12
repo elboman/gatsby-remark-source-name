544cf9b8c671ea1fbe017b5945f4360eb0230e1f
Marco Botto <marco.botto@gmail.com>
2018-05-12 02:53:36 +0100

    feat(*): first release

---
+++ src/gatsby-node.js
@@ -0,0 +1,16 @@
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators;
+
+  // We only care about MarkdownRemark content.
+  if (node.internal.type !== 'MarkdownRemark') {
+    return;
+  }
+
+  const fileNode = getNode(node.parent);
+
+  createNodeField({
+    node,
+    name: 'sourceName',
+    value: fileNode.sourceInstanceName,
+  });
+};
